317ce54e38c6ab9091af8421cbcaa4acee59a763
José Amendola <amendola.andrade@hotmail.com>
2018-04-25 22:59:26 -0300

    batch_processing

---
+++ process_tuples.py
@@ -0,0 +1,50 @@
+from simulation_settings import *
+import matplotlib.pyplot as plt
+import pickle
+import os
+import reward
+
+if __name__ == '__main__':
+
+    dir_name = './dyna/samples'
+    files = os.listdir(dir_name)
+    transitions = list()
+    batch_list = list()
+    for file in files:
+        with open(os.path.join(dir_name, file), 'rb') as infile:
+            print('Loading file:',file)
+            try:
+                while True:
+                    transitions = pickle.load(infile)
+            except EOFError as e:
+                pass
+        print('Number of transitions added : ', len(transitions))
+        batch_list = batch_list + transitions
+    states = [tuple[0] for tuple in batch_list]
+    states_p = [tuple[2] for tuple in batch_list]
+    pos_x = [state[0] for state in states]
+    pos_y = [state[1] for state in states]
+    pos_x_p = [state[0] for state in states_p]
+    pos_y_p = [state[1] for state in states_p]
+    plt.xlim(0, 12000)
+    plt.ylim(2000, 8000)
+    ax = plt.axes()
+
+    replace_reward = reward.RewardMapper(plot_flag=False, r_mode_='exp_border_target_rot_angle')
+    replace_reward.set_boundary_points(buoys)
+    replace_reward.set_goal(goal, goal_heading_e_ccw, goal_vel_lon)
+    new_list = replace_reward.replace_reward(batch_list)
+
+
+    # ax.arrow(0, 0, 0.5, 0.5, head_width=0.05, head_length=0.1, fc='k', ec='k')
+    # plt.draw()
+    # plt.pause(2)
+    for i,st in enumerate(states):
+        ax.arrow(pos_x[i] ,pos_y[i], (pos_x_p[i] - pos_x[i]), (pos_y_p[i] - pos_y[i]))
+
+
+    # x_pos = [state[0] for state in states if state[0] < 12000]
+    # y_pos = [state[1] for state in states if state[0] < 12000]
+    # plt.plot(x_pos, y_pos,'g^')
+
+
---
+++ simulation_settings.py
@@ -0,0 +1,33 @@
+import utils
+
+variables_file = "experiment_" + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+learner_file = "agent" + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+sample_file = "samples/samples" + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+q_file = "q_table" + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+main_loop_iterations = 10
+max_fit_iterations = 2000
+max_tuples_per_batch = 20000000
+maximum_training_steps = 20000000
+evaluation_steps = 1000
+max_episodes = 5000000
+steps_between_actions = 100
+funnel_start = (14000, 7000)
+N01 = (11724.8015, 5582.9127)
+N03 = (9191.6506, 4967.8532)
+N05 = (6897.7712, 4417.3295)
+N07 = (5539.2417, 4089.4744)
+Final = (5812.6136, 3768.5637)
+N06 = (6955.9288, 4227.1846)
+N04 = (9235.8653, 4772.7884)
+N02 = (11770.3259, 5378.4429)
+funnel_end = (14000, 4000)
+plot = False
+goal_heading_e_ccw = utils.channel_angle_e_ccw(N03, N05)
+goal_vel_lon = 1.5
+buoys = (funnel_start, N01, N03, N05, N07, Final, N06, N04, N02, funnel_end)
+vessel_id = '36'
+rudder_id = '0'
+thruster_id = '0'
+scenario = 'default'
+goal = ((N07[0]+Final[0])/2, (N07[1]+Final[1])/2)
+goal_factor = 100
