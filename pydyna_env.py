e467de2e218add8557b425b8ad827447790b3991
jose_amendola <jose_amendola@tpn.usp.br>
2018-12-02 18:06:23 +0000

    fix_original_state

--- pydyna_env.py
+++ pydyna_env.py
@@ -24,8 +24,8 @@ class PyDynaEnv(Env):
                                  (self.goal[0] + self.goal_factor, self.goal[1] + self.goal_factor),
                                  (self.goal[0] + self.goal_factor, self.goal[1] - self.goal_factor)))
         self.action_space = spaces.Discrete(3)
-        self.observation_space = spaces.Box(low=np.array([-1.0, -180, -1.0, 0.5, 0.0]),
-                                            high=np.array([1.0, -50, 1.0, 4.5, 20000.0]))
+        self.observation_space = spaces.Box(low=np.array([-1.0, -180, -1.0, 0.0]),
+                                            high=np.array([1.0, -150, 1.0, 20000]))
         self.last_pos = np.zeros(3) # last_pos = [xg yg thg]
         self.last_action = np.zeros(2)
         self.simulator = FastTimeWrapper()
@@ -40,10 +40,11 @@ class PyDynaEnv(Env):
         self.viewer = None
         self.last_rew = 0
         self.symmetry = 1
+        self.original_state = self.simulator.get_vessel_pos_vel()
 
     def step(self, action):
         rudder_action, rot_action = self.convert_action(action)
-        rudder_action *= self.symmetry
+        # rudder_action *= self.symmetry
         print('Rudder lvl: ', rudder_action)
         print('Rot lvl: ', rot_action)
         self.simulator.set_norm_dem_rudder_level(rudder_action)
@@ -55,21 +56,24 @@ class PyDynaEnv(Env):
         # transforma variáveis do simulador em variáveis observáveis
         obs = self.convert_state_sog_cog(state_prime)
         print('Observed state: ', obs)
-        if obs[0] < 0:
-            obs[0], obs[1], obs[2] = - obs[0], -166.6-(obs[1]+166.6), -obs[2]
-            self.symmetry = -1
-            print('Inverted symmetry signal!!')
-            print('Reflected obs: ', obs)
-        else:
-            self.symmetry = 1
+        # if obs[0] < 0:
+        #     obs[0], obs[1], obs[2] = - obs[0], -166.6-(obs[1]+166.6), -obs[2]
+        #     self.symmetry = -1
+        #     print('Inverted symmetry signal!!')
+        #     print('Reflected obs: ', obs)
+        # else:
+        #     self.symmetry = 1
         print('Action: ', action)
         theta = self.angle_from_dyna(state_prime[5])
-        self.ship_polygon = affinity.translate(self.ship_polygon, state_prime[0]-self.last_pos[0], state_prime[1]-self.last_pos[1])
-        self.ship_polygon = affinity.rotate(self.ship_polygon, -(theta-self.last_pos[2]), 'center')
+        # self.ship_polygon = affinity.translate(self.ship_polygon, state_prime[0]-self.last_pos[0], state_prime[1]-self.last_pos[1])
+        # self.ship_polygon = affinity.rotate(self.ship_polygon, -(theta-self.last_pos[2]), 'center')
         dn = self.end(state_prime=state_prime, obs=obs)
         # rew = self.calculate_reward(obs=obs)
         rew = self.calculate_reward(obs)
         self.last_rew = rew
+        # if dn:
+        #     if not self.goal_rec.contains(self.ship_point):
+        #         rew -= 1000000*obs[3]
         if dn:
             if not self.goal_rec.contains(self.ship_point):
                 rew -= 1000
@@ -80,21 +84,23 @@ class PyDynaEnv(Env):
         return obs, rew, dn, info
 
     def calculate_reward(self, obs):
-            rew = -np.abs(obs[1] + 166.6)**2 - 1000*np.abs(obs[2])**2 - np.abs(obs[3])**2 + 5000/obs[4]
-            return rew
+        rew = (1-np.abs(obs[1] + 166.6)/5) * (1 - 1000 * np.abs(obs[2]) ** 2)
+        # rew = (1-(np.abs(obs[1] + 166.6)/5))*(1 - 10*np.abs(obs[2]))/obs[3]
+        return rew
 
     def convert_state_sog_cog(self, state):
         theta_deg = self.angle_from_dyna(state[5])
         self.ship_point = Point((state[0], state[1]))
         bank_balance = (self.ship_point.distance(self.upper_line) - self.ship_point.distance(self.lower_line)) / \
                        (self.ship_point.distance(self.upper_line) + self.ship_point.distance(self.lower_line))
-        sog = np.linalg.norm([state[6], state[7]])
+        # sog = np.linalg.norm([state[6], state[7]])
         cog = np.degrees(np.arctan2(state[7], state[6]))
         goal_dist = self.goal_point.distance(self.ship_point)
+        # drift_angle = -self.angle_from_dyna(state[5])-270.0 - cog
         if cog > 0:
             cog -= 360
         rate_of_turn = -np.rad2deg(state[11])
-        obs = np.array([bank_balance, cog, rate_of_turn, sog, goal_dist])
+        obs = np.array([bank_balance, cog, rate_of_turn, goal_dist])
         # print('Observation', obs)
         return obs
 
@@ -142,13 +148,13 @@ class PyDynaEnv(Env):
             return False
 
     def reset(self):
-        original_state = self.simulator.get_vessel_pos_vel()
         init_pos = [11000, 5280]
+        # init_pos = [9984, 4980]
         theta = np.deg2rad(90-(-103.4))
         init_vel = 3
-        init = [init_pos[0], init_pos[1], original_state[2], original_state[3], original_state[4], theta,
-                init_vel*np.cos(theta), init_vel*np.sin(theta), original_state[8],
-                original_state[9], original_state[10], original_state[11]]
+        init = [init_pos[0], init_pos[1], self.original_state[2], self.original_state[3], self.original_state[4], theta,
+                init_vel*np.cos(theta), init_vel*np.sin(theta), self.original_state[8],
+                self.original_state[9], self.original_state[10], self.original_state[11]]
         self.simulator.reset_vessel_state(init)
         print('Reseting position')
         state = self.simulator.get_vessel_pos_vel()
